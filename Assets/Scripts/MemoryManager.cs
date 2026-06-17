1b99cd9b74d8cd25e18ad91ef2b00e2689e8f167
leamtx <lea.monthieux@epitech.eu>
2026-06-17 10:55:54 +0200

    feat: add memory manager

---
+++ Assets/Scripts/MemoryManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MemoryManager : MonoBehaviour
+{
+   private void Start()
+    {
+        Debug.Log("Memory task started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MemoryObject.cs
+++ Assets/Scripts/MemoryObject.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 
-public class Script : MonoBehaviour
+public class MemoryObject : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float visibleDuration = 10f;
 
+    public Vector3 OriginalPosition { get; private set; }
+
     private void Start()
     {
+        OriginalPosition = transform.position; // enregistre la position du transform ( composé de position, scale, rotation) du cube actuel (-2 x, 0.5 y , 2 z)
+//conservé en mémoire pour savoir où était le cube avant de disparaitre, calcul erreur joueur ( comparaison position choisie, position réelle)
         Invoke(nameof(HideObject), visibleDuration);
     }
 
